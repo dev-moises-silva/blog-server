c8ea60dd925bc25b7d1e8d4aa4ab8e90be7a76c0
Moisés Silva <josemoisesdasilvalima@gmail.com>
2024-11-21 13:56:40 -0300

    feat: Post creation event created

---
+++ app/Events/PostCreated.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Events;
+
+use App\Http\Resources\PostResource;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class PostCreated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct
+    (
+        public PostResource $post
+    )
+    {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): PrivateChannel
+    {
+        return new PrivateChannel('posts');
+    }
+}
--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('posts', function () {
+    return Auth::check();
 });
